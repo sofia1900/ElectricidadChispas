83603214aec534d0f9719a21bd2861f82d6c1945
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-11-19 13:00:10 +0100

    Parte del proyecto sobre la impresion de la factura

--- src/InvoicePrinter.java
+++ src/InvoicePrinter.java
@@ -10,22 +10,18 @@ public class InvoicePrinter {
                 invoice.getCompany().getPostalAddress() + " | " + invoice.getCompany().getCity() + " | " +
                 invoice.getCompany().getProvince() + " | " + invoice.getCompany().getEmail());
 
-
-        double total = 0.0; // variable para ir acomulando el valor total
         System.out.println("Linea de venta (producto|servicio): Codigo | Nombre | Precio | tipo IVA | Total");
         for (Integer i = 0 ; i < invoice.getProduct().size() ; i++){
             System.out.println(invoice.getProduct().get(i).getCode() + " | " + invoice.getProduct().get(i).getName() + " | " +
                     invoice.getProduct().get(i).getPrice() + " | " + invoice.getProduct().get(i).getVatRate().getType() + " | " +
-                    invoice.getProduct().get(i).getVatRate().getValue()* invoice.getProduct().get(i).getPrice()/100);
-            total = total + invoice.getProduct().get(i).getVatRate().getValue()* invoice.getProduct().get(i).getPrice()/100;
+                    invoice.getProduct().get(i).getPrice()+(invoice.getProduct().get(i).getVatRate().getValue()*invoice.getProduct().get(i).getPrice()/100));
         }
         for (Integer i = 0 ; i < invoice.getServices().size() ; i++){
             System.out.println(invoice.getServices().get(i).getCode() + " | " + invoice.getServices().get(i).getName() + " | " +
                     invoice.getServices().get(i).getPrice() + " | " + invoice.getServices().get(i).getVatRate().getType() + " | " +
-                    invoice.getServices().get(i).getVatRate().getValue()* invoice.getServices().get(i).getPrice()/100);
-            total = total + invoice.getServices().get(i).getVatRate().getValue()* invoice.getServices().get(i).getPrice()/100;
+                    invoice.getServices().get(i).getPrice() + (invoice.getServices().get(i).getVatRate().getValue()*invoice.getServices().get(i).getPrice()/100));
         }
-        System.out.println("TOTAL = " + total);
+        System.out.println("TOTAL = " + invoice.getTotal());
 
 
 
