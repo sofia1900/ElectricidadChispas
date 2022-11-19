e2fb7455d1f0ec65d59b19b171f014d53b9b47bd
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-11-19 12:46:57 +0100

    Parte del proyecto sobre la factura

--- src/InvoicePrinter.java
+++ src/InvoicePrinter.java
@@ -1,5 +1,32 @@
 public class InvoicePrinter {
     public void print(Invoice invoice){
+        System.out.println("Factura: " + invoice.getCode() + "|" + invoice.getDate());
+
+        //Falta un IF para saber si ha introducido un autonomo o una soliciedad
+        System.out.println("Cliente: " + invoice.getSelfEmployed().getIdNumber() + " | " + invoice.getSelfEmployed().getFullName() +
+                " | " + invoice.getSelfEmployed().getPostalAddress() + " | " + invoice.getSelfEmployed().getCity() + " | " +
+                invoice.getSelfEmployed().getProvince()+ " | " + invoice.getSelfEmployed().getEmail());
+        System.out.println("Cliente: " + invoice.getCompany().getIdNumber()+ " | " + invoice.getCompany().getName()+ " | " +
+                invoice.getCompany().getPostalAddress() + " | " + invoice.getCompany().getCity() + " | " +
+                invoice.getCompany().getProvince() + " | " + invoice.getCompany().getEmail());
+
+
+        double total = 0.0; // variable para ir acomulando el valor total
+        System.out.println("Linea de venta (producto|servicio): Codigo | Nombre | Precio | tipo IVA | Total");
+        for (Integer i = 0 ; i < invoice.getProduct().size() ; i++){
+            System.out.println(invoice.getProduct().get(i).getCode() + " | " + invoice.getProduct().get(i).getName() + " | " +
+                    invoice.getProduct().get(i).getPrice() + " | " + invoice.getProduct().get(i).getVatRate().getType() + " | " +
+                    invoice.getProduct().get(i).getVatRate().getValue()* invoice.getProduct().get(i).getPrice()/100);
+            total = total + invoice.getProduct().get(i).getVatRate().getValue()* invoice.getProduct().get(i).getPrice()/100;
+        }
+        for (Integer i = 0 ; i < invoice.getServices().size() ; i++){
+            System.out.println(invoice.getServices().get(i).getCode() + " | " + invoice.getServices().get(i).getName() + " | " +
+                    invoice.getServices().get(i).getPrice() + " | " + invoice.getServices().get(i).getVatRate().getType() + " | " +
+                    invoice.getServices().get(i).getVatRate().getValue()* invoice.getServices().get(i).getPrice()/100);
+            total = total + invoice.getServices().get(i).getVatRate().getValue()* invoice.getServices().get(i).getPrice()/100;
+        }
+        System.out.println("TOTAL = " + total);
+
 
 
     }
