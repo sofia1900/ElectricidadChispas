6074b2b0b2e7946a5957a3024a75b774384f52e8
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-11-19 13:01:34 +0100

    Parte del proyecto sobre las lineas de venta: productos y servicios

--- src/VatRate.java
+++ src/VatRate.java
@@ -1,12 +1,9 @@
 public class VatRate {
     //Atributos
-    private String type;
-    private Integer value;
+    private Integer type;
 
     //Metodos
-    public String getType(){return type;}
-    public void setType(String type){this.type = type;}
+    public Integer getType(){return type;}
+    public void setType(Integer type){this.type = type;}
 
-    public Integer getValue(){return value;}
-    public void setValue(Integer value){this.value = value;}
 }
