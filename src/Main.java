396c78b7917f4f51a411eade7b91d48829866149
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-11-21 19:35:06 +0100

    Parte del proyecto sobre la factura

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 import java.util.Date;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main (String[] args) throws ParseException{
 
         Scanner scanner = new Scanner(System.in);
 
