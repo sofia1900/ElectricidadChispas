688923342c53320b06edb6bcca957bbe3b3ba0f3
sofia1900 <115726194+sofia1900@users.noreply.github.com>
2022-11-21 13:17:51 +0100

    Parte del proyecto sobre la impresion de la factura

--- src/InvoicePrinter.java
+++ src/InvoicePrinter.java
@@ -15,15 +15,15 @@ public class InvoicePrinter {
         System.out.println("Linea de venta (producto|servicio): Codigo | Nombre | Precio | tipo IVA | Total");
         for (Integer i = 0 ; i < invoice.getProduct().size() ; i++){
             System.out.println(invoice.getProduct().get(i).getCode() + " | " + invoice.getProduct().get(i).getName() + " | " +
-                    invoice.getProduct().get(i).getPrice() + " | " + invoice.getProduct().get(i).getVatRate().getType() + " | " +
-                    (invoice.getProduct().get(i).getPrice()+(invoice.getProduct().get(i).getVatRate().getType()*invoice.getProduct().get(i).getPrice()/100)));
+                    invoice.getProduct().get(i).getPrice() + "€" + " | " + invoice.getProduct().get(i).getVatRate().getType() +"%"+ " | " +
+                    (invoice.getProduct().get(i).getPrice()+(invoice.getProduct().get(i).getVatRate().getType()*invoice.getProduct().get(i).getPrice()/100))+"€");
         }
         for (Integer i = 0 ; i < invoice.getServices().size() ; i++){
             System.out.println(invoice.getServices().get(i).getCode() + " | " + invoice.getServices().get(i).getName() + " | " +
-                    invoice.getServices().get(i).getPrice() + " | " + invoice.getServices().get(i).getVatRate().getType() + " | " +
-                    (invoice.getServices().get(i).getPrice() + (invoice.getServices().get(i).getVatRate().getType()*invoice.getServices().get(i).getPrice()/100)));
+                    invoice.getServices().get(i).getPrice() +"€"+ " | " + invoice.getServices().get(i).getVatRate().getType() +"%"+ " | " +
+                    (invoice.getServices().get(i).getPrice() + (invoice.getServices().get(i).getVatRate().getType()*invoice.getServices().get(i).getPrice()/100))+"€");
         }
-        System.out.println("TOTAL = " + invoice.getTotal());
+        System.out.println("TOTAL = " + invoice.getTotal() +"€ \n");
     }
     
 }
